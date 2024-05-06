62462e9c207f2c2ca49c59a54572a9fdb096b165
MandlaDev <mandlamwangu@gmail.com>
2024-05-06 22:58:50 +0200

    account page user input functionality added

--- src/app/pages/tabs/account/account.page.ts
+++ src/app/pages/tabs/account/account.page.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
+  name: string = ''; // Initialize name input
+  phoneNumber: string = ''; // Initialize phone number input
+  emailAddress: string = ''; // Initialize email input
 
   constructor() { }
 
   ngOnInit() {
+    // Load account details from local storage when the component is initialized
+    this.loadAccountDetails();
   }
 
+  loadAccountDetails() {
+    // Retrieve account details from local storage
+    const storedName = localStorage.getItem('name');
+    if (storedName) {
+      this.name = storedName;
+    }
+
+    const storedPhoneNumber = localStorage.getItem('phoneNumber');
+    if (storedPhoneNumber) {
+      this.phoneNumber = storedPhoneNumber;
+    }
+
+    const storedEmailAddress = localStorage.getItem('emailAddress');
+    if (storedEmailAddress) {
+      this.emailAddress = storedEmailAddress;
+    }
+  }
+
+  saveAccountDetails() {
+    // Save account details to local storage
+    localStorage.setItem('name', this.name);
+    localStorage.setItem('phoneNumber', this.phoneNumber);
+    localStorage.setItem('emailAddress', this.emailAddress);
+  }
 }
